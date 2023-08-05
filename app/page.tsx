8c6f8aabf2bf3ab1b016537195f28e7bc2ff04dd
devRabbani <dev.golamrabbani@gmail.com>
2023-08-05 22:40:17 +0530

    📦 NEW: added auth persistence with db

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
+import { authOptions } from './api/auth/[...nextauth]/route'
 
 export default async function Home() {
-  const session = await getServerSession()
+  const session = await getServerSession(authOptions)
+
+  console.log('Sesssssssion', session)
 
   if (!session) {
     redirect('api/auth/signin')
